81b74347ece30ea8ffdf49466323a3a415b3cffc
Daniel Țoncu <Daniel.Toncu@centric.eu>
2020-01-09 16:18:42 +0200

    Add Info-Command

---
+++ src/commands/base.py
@@ -0,0 +1,19 @@
+"""
+"""
+
+
+class BaseCommand:
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+
+        pass
+
+    def execute(self):
+        """
+        """
+
+        raise NotImplementedError()
---
+++ src/commands/info.py
@@ -0,0 +1,50 @@
+"""
+"""
+
+from commands.base import BaseCommand
+
+from helpers.files_helper import set_file_content
+
+
+class InfoCommand(BaseCommand):
+    """
+    """
+
+    NAME = "info"
+
+    DATA = {
+        "Project": "Anti-Spam Filter",
+        "StudentName": "Daniel Țoncu",
+        "Alias": "Heisenberg",
+        "Version": "0.9"
+    }
+
+    def __init__(self, output_file=None):
+        """
+        """
+
+        super(InfoCommand, self).__init__()
+
+        self._output_file = output_file
+
+    def _get_info(self):
+        """
+        """
+
+        return "%s\n%s\n%s\n%s" % (
+            InfoCommand.DATA["Project"],
+            InfoCommand.DATA["StudentName"],
+            InfoCommand.DATA["Alias"],
+            InfoCommand.DATA["Version"]
+        )
+
+    def execute(self):
+        """
+        """
+
+        if self._output_file is None:
+            return self._get_info()
+
+        set_file_content(self._output_file, self._get_info())
+
+        return ""
